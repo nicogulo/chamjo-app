c25ac7df9bc084f2fad18b5021b80cbb516be0f0
nicogulo <nicogulobelas@gmail.com>
2024-05-11 10:41:05 +0700

    chore: next config svg

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        dangerouslyAllowSVG: true,
+        contentDispositionType: "attachment",
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ["@svgr/webpack"]
+        })
+
+        return config
+    }
+}
 
 module.exports = nextConfig
