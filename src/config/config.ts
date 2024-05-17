1e27ed73249ba799f420e9cbfd011f743bfac379
nicogulo <nicogulobelas@gmail.com>
2024-05-18 02:05:26 +0700

    chore: config env export

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,3 +4,5 @@ export const SUPABASE_URL = process.env.NEXT_SUPABASE_URL
 export const SUPABASE_PUBLIC_KEY = process.env.SUPABASE_PUBLIC_KEY
 export const PUBLIC_SITE = process.env.NEXT_PUBLIC_SITE_URL
 export const PUBLIC_VERCEL_URL = process.env.PUBLIC_VERCEL_URL
+export const COOKIE_DOMAIN = process.env.NEXT_PUBLIC_COOKIE_DOMAIN
+export const COOKIE_PREFIX = process.env.NEXT_PUBLIC_COOKIE_PREFIX
