67ac744f30ab27443d66e7c2e641c29edc3f62ca
nicogulo <nicogulobelas@gmail.com>
2024-05-26 01:07:14 +0700

    chore: add hostname image

--- next.config.js
+++ next.config.js
@@ -9,6 +9,11 @@ const nextConfig = {
             {
                 protocol: "https",
                 hostname: "lh3.googleusercontent.com"
+            },
+
+            {
+                protocol: "https",
+                hostname: "ndbqcbbgigoygotysyae.supabase.co"
             }
         ]
     },
