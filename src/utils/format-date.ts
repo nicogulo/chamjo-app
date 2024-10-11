58b620dbe7de1e189700ce696601cd6e0bf790fc
nicogulo <nicogulobelas@gmail.com>
2024-10-12 00:48:56 +0700

    feat(utils): format date

---
+++ src/utils/format-date.ts
@@ -0,0 +1,23 @@
+import dayjs, { Dayjs } from "dayjs"
+import localeData from "dayjs/plugin/localeData"
+
+import "dayjs/locale/id"
+export type LanguagesCode = "id" | "en"
+
+export const formatDate = (
+    date: number | Date | string | Dayjs | null,
+    format: string,
+    locale: LanguagesCode = "en"
+): string => {
+    dayjs.extend(localeData)
+
+    if (typeof date === "number") {
+        const { length } = date.toString()
+
+        if (length === 10) {
+            return dayjs.unix(date).format(format)
+        }
+    }
+
+    return dayjs(date).locale(locale).format(format)
+}
