1339474886b43f9323e8715b95aaa11ed8dfb379
nicogulo <nicogulobelas@gmail.com>
2024-10-13 01:58:44 +0700

    refactor: config tailwind color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,33 +12,49 @@ const config: Config = {
                 transparent: "transparent",
                 current: "currentColor",
                 base: {
-                    1: "#FAFAFB",
-                    2: "#F5F6F7",
-                    3: "#EBECEF",
-                    4: "#C3C7CE",
-                    5: "#9AA1AD",
-                    6: "#727C8C",
-                    7: "#4A576B",
-                    8: "#21314A",
-                    9: "#031531"
+                    50: "#FFFFFF",
+                    100: "#FAFAFB",
+                    200: "#F5F6F8",
+                    300: "#F1F2F4",
+                    400: "#ECECEC",
+                    500: "#C3C7CE",
+                    600: "#9AA1AD",
+                    700: "#727C8C",
+                    800: "#5E636F",
+                    900: "#222222"
                 },
                 primary: {
-                    1: "#FDEBD9",
-                    2: "#FBD3B5",
-                    3: "#F5B28E",
-                    4: "#EC9270",
-                    5: "#E06343",
-                    6: "#C04330",
-                    7: "#A12821",
-                    8: "#811517",
-                    9: "#6B0C15"
+                    50: "#FCF2ED",
+                    100: "#F7D8C9",
+                    200: "#F2BEA6",
+                    300: "#EDA482",
+                    400: "#E88A5E",
+                    500: "#E1662F",
+                    600: "#D6591F",
+                    700: "#B34A19",
+                    800: "#8F3B14",
+                    900: "#6B2C0F"
+                },
+                secondary: {
+                    50: "#F2EEFC",
+                    100: "#E9D9FD",
+                    200: "#D2B5FB",
+                    300: "#B68EF5",
+                    400: "#9B70EB",
+                    500: "#7543DF",
+                    600: "#5A30BF",
+                    700: "#4221A0",
+                    800: "#2D1581",
+                    900: "#1E0C6B"
+                },
+                transparant: {
+                    primary: "#E1662F14",
+                    secondary: "#7543DF14"
+                },
+                brand: {
+                    1: "#E06343",
+                    2: "#562A2C"
                 }
-            },
-            borderRadius: {
-                none: "0",
-                sm: "6px",
-                DEFAULT: "12px",
-                md: "16px"
             },
             fontSize: {
                 1: "12px",
